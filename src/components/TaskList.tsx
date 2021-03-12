30b47de44339f411d15acc94975cd36119f50d50
Túlio Ribeiro <tuliocribeiro@gmail.com>
2021-03-12 18:23:14 -0300

    test complete: should be able to add a task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,18 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return false;
+    }
+
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+    ]);
   }
 
   function handleToggleTaskCompletion(id: number) {
