1b631daa910a3bf4e647cde2129298864bfc11a0
Túlio Ribeiro <tuliocribeiro@gmail.com>
2021-03-12 18:26:36 -0300

    test complete: should be able to check a task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -32,6 +32,13 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete
+      }
+    
+      return task;
+    }));    
   }
 
   function handleRemoveTask(id: number) {
